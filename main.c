826adfaa7bacfbeec28512967fe8c654b6c5644d
Benett <williambenet@hotmail.com>
2025-06-03 01:23:44 -0400

    Timeout added for faster response on the TCP Scan

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/time.h>
 
 int scan_port(const char *target_ip, int port) {
     int sock;
@@ -13,6 +15,13 @@ int scan_port(const char *target_ip, int port) {
         perror("Socket creation failed");
         return -1;
     }
+
+    struct timeval timeout;
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
+
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
+    setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout));
 
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
@@ -28,8 +37,8 @@ int scan_port(const char *target_ip, int port) {
 }
 
 int main(int argc, char *argv[]) {
-    if(argc != 4){
-        printf("Usage: %s <target IP> <start port> <end port> \n", argv[0]);
+    if (argc != 4) {
+        printf("Usage: %s <target IP> <start port> <end port>\n", argv[0]);
         return 1;
     }
 
@@ -37,14 +46,22 @@ int main(int argc, char *argv[]) {
     int start_port = atoi(argv[2]);
     int end_port = atoi(argv[3]);
 
-     for (int port = start_port; port <= end_port; port++) {
+    if (start_port < 1 || end_port > 65535 || start_port > end_port) {
+        printf("Invalid port range. Ports must be between 1 and 65535.\n");
+        return 1;
+    }
+
+    printf("Scanning %s from port %d to %d...\n\n", target_ip, start_port, end_port);
+
+    for (int port = start_port; port <= end_port; port++) {
         int result = scan_port(target_ip, port);
         if (result == 1) {
-            printf("Port %d is OPEN\n", port);
+            printf("[+] Port %d is OPEN\n", port);
         } else {
-            printf("Port %d is closed\n", port);
+            printf("[-] Port %d is closed\n", port);
         }
     }
 
+    printf("\nScan complete.\n");
     return 0;
 }
